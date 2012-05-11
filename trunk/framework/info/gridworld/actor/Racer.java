0c257099db4899ae23a1d3415e4a4a5a97abbf45
mooney.jake@gmail.com <mooney.jake@gmail.com@e1760fef-0dea-2bee-bdb1-d1e79464e8aa>
2012-05-11 11:56:48 +0000

    racer class added

--- trunk/framework/info/gridworld/actor/Racer.java
+++ trunk/framework/info/gridworld/actor/Racer.java
@@ -14,7 +14,31 @@ public class Racer extends Bug{
 	        if (gr.isValid(next))
 	            moveTo(next);
 	        else
-	            removeSelfFromGrid();
-
+	            throw new IllegalArgumentException("Lost");
 	}
+	
+    public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null);
+        // ok to move into empty location
+        // not ok to move onto any other actor or wall
+    }
+    
+    public void act()
+    {
+        if (canMove()){
+            move();
+        }
+        else{
+            throw new IllegalArgumentException("Lost");
+        }
+    }
 }
